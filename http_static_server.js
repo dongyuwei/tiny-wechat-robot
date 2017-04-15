d56353e11aa8d57bbeb594ecd39614b571a6134b
Dong Yuwei <newdongyuwei@gmail.com>
2017-04-15 09:24:51 +0800

    extract injected code to a standalone file

--- http_static_server.js
+++ http_static_server.js
@@ -4,12 +4,12 @@ const fs = require('fs');
 const server = http.createServer((req, res) => {
     console.log(req.url);
     if (req.url.indexOf('/hijacked_wechat_index.js') !== -1) {
-        fs.readFile('./hijacked_wechat_index.js', 'utf-8', function(err, content){
-            res.writeHead(200, {
-                'Cache-Control': 'no-cache',
-                'Content-Type': 'application/x-javascript; charset=utf-8' });
-            res.end(content);
-        });
+        res.writeHead(200, {
+            'Cache-Control': 'no-cache',
+            'Content-Type': 'application/x-javascript; charset=utf-8'}
+        );
+        res.write(fs.readFileSync('./wechat_injection.js', 'utf-8'));
+        res.end(fs.readFileSync('./hijacked_wechat_index.js', 'utf-8'));
     } else {
         res.end(req.url);
     }
